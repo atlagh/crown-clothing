7024ac9d2375a68e130ebd7e99b5cf9cd9f759ec
amine <atlagh.amine@gmail.com>
2022-08-21 13:27:48 +0100

    minor change

--- src/components/login/login.component.jsx
+++ src/components/login/login.component.jsx
@@ -35,8 +35,6 @@ class LogIn extends React.Component {
             <div className='log-in' >
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
-
-
                 <form onSubmit={this.handleSubmit}>
                     <FormInput name="email" type="email" lable="Email" handleChange={this.handleChange} value={this.state.email} required />
                     
